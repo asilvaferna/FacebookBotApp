40f8fc9c030e0331bffde9aabaf3a3d508bef66c
adri4silva <adri4silva@gmail.com>
2017-04-27 09:46:51 +0200

    Main class to execute all windows

---
+++ facebookapp/FacebookApp.java
@@ -0,0 +1,27 @@
+package facebookapp;
+
+/**
+ * <h1>Facebook APP</h1>
+ * This program contains a Java Facebook application.
+ * <p>
+ * This class contains the main class, that just calls the others
+ * class.
+ * @author Adri
+ * @version 0.1.0
+ * @since 05-27-2017
+ */
+public class FacebookApp {
+
+    /**
+     * @author Adri
+     * @version 0.1.0
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window w1 = new Window();
+        
+        w1.setVisible(true);
+
+    }
+    
+}
